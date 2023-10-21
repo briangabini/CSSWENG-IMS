3c4ec50dc80270b1495a0fd9df82cdf4ada8f44d
Brian Gabini <91449132+nriab-dlsu@users.noreply.github.com>
2023-10-21 13:08:01 +0800

    Configured MERN stack

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,27 +1,30 @@
-// import {useEffect, useState} from 'react'
+import {useEffect, useState} from 'react'
+
+// components 
+import InventoryItemDetails from '../components/InventoryItemDetails'
 
 const Home = () => {
-    // const [inventoryItems, setInventoryItems] = useState(null)
+    const [inventoryItems, setInventoryItems] = useState(null)
 
-    // useEffect(() => {
-    //     const fetchInventoryItems = async () => {
-    //         const response = await fetch('/jpd')
-    //         const json = await response.json() // converts the json data into an array of objects
+    useEffect(() => {
+        const fetchInventoryItems = async () => {
+            const response = await fetch('/jpd')
+            const json = await response.json() // converts the json data into an array of objects
 
-    //         if (response.ok) {
-    //             setInventoryItems(json)
-    //         }
-    //     }
+            if (response.ok) {
+                setInventoryItems(json)
+            }
+        }
 
-    //     fetchInventoryItems()
-    // }, [])
+        fetchInventoryItems()
+    }, [])
 
     return (
         <div className="home">
             <div className="inventory-items">
-                {/* {inventoryItems && inventoryItems.map((inventoryItem) => (
-                    <p key={inventoryItem._id}>{inventoryItem.partName}</p>
-                ))} */}
+                {inventoryItems && inventoryItems.map((inventoryItem) => (
+                    <InventoryItemDetails key={inventoryItem._id} inventoryItem={inventoryItem}/>
+                ))}
             </div>
         </div>
     )
