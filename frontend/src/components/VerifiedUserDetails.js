e9f853fe24a85c46248b4d0f06e8b0f55da6e27f
CarlMatthewVerano <113345631+CarlMatthewVerano@users.noreply.github.com>
2023-10-24 00:14:57 +0800

    Implemented Search Functionality (Tested and working)

--- frontend/src/components/VerifiedUserDetails.js
+++ frontend/src/components/VerifiedUserDetails.js
@@ -3,7 +3,7 @@ const VerifiedUserDetails = ({userDetail}) => {
     return (
         <div className="verified-user-details">
             <p>Employee Name: {userDetail.employeeName}</p>
-            <p>Email Address: {userDetail.emailAddress}</p>
+            <p>Email Address: {userDetail.email}</p>
             <p>Role: {userDetail.role}</p>
             <p>Date Added: {userDetail.dateAdded}</p>
         </div>
