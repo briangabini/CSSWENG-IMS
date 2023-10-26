251dc7ec9edeaf5c8a03fa88bc66098d404954fe
Denis Leeroi Villamiel <denisvillamiel@gmail.com>
2023-10-26 10:16:21 +0800

    updated sidebar.js again

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 
@@ -11,7 +12,7 @@ function Sidebar() {
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
-        Launch
+        <p>=</p>
       </Button>
 
       <Offcanvas show={show} onHide={handleClose}>
@@ -21,6 +22,38 @@ function Sidebar() {
         <Offcanvas.Body>
           Some text as placeholder. In real life you can have the elements you
           have chosen. Like, text, images, lists, etc.
+
+          {/* Links to the inventory page */}
+          <Link to="/inventory">
+                <h4>Inventory</h4>
+            </Link>
+
+            {/* Links to the finance page */}
+            <Link to="/finance">
+                <h4>Finance</h4>
+            </Link>
+
+            
+            {/* Links to the add item page */}
+            <Link to="/add-item">
+                <h4>Add Item</h4>
+            </Link>
+
+            {/* Links to the check out page */}
+          <Link to="/check-out">
+                <h4>Check Out</h4>
+            </Link>
+
+            {/* Links to the shopping cart */}
+            <Link to="/audit-log">
+                <h4>Audit Log</h4>
+            </Link>
+
+            {/** ADD IF STATEMENT TO CHECK IF USER IS ADMIN OR NOT */}
+            {/* Links to the login page*/}
+            <Link to="/admin-control-center">
+                <h4>Admin Control Center</h4>
+            </Link>
         </Offcanvas.Body>
       </Offcanvas>
     </>
