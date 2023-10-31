4e8c5e6f500ff4805be78bea441db342f3b841e8
frncs-dc <97236341+frncs-dc@users.noreply.github.com>
2023-10-31 22:58:34 +0800

    Update Dashboard.js
    fixed Revenue card; can now click the dropdown button without navigating to Sales page

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,10 +1,19 @@
 // bootstrap
-import {Container, Row, Col, Card, Dropdown, DropdownButton, Button} from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import {Container, Row, Col, Card, Dropdown, DropdownButton, Button, Form} from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 import ReminderAndStatistics from '../components/ReminderAndStatistics'
 
 const Dashboard = () => {
 
+    const navigate = useNavigate();
+
+    const navigateSales = () => {
+        navigate(`/sales-page`);
+    };
+
+    const handleDropdownClick = (e) => {
+        e.stopPropagation();
+    }
     return (
        
 
@@ -15,34 +24,32 @@ const Dashboard = () => {
 
             <Row className='pe-2'>
                 <Col className='col-8'>
-                    <Link to="/sales-page" style={{textDecoration:'none', margin:0, padding:0, color:'black'}}>
-                        <Row className='fw-bold txt-20 my-2'>
-                            Revenue
-                        </Row>
-                        <Row className='me-2'>
-                            <Card className='p-4 bg-main-dominant-red overlay-revenue'>
-                                <Row>
-                                    <DropdownButton id="dropdown-item-button" title="Month" align="end"
-                                                    className='d-flex flex-row-reverse'
-                                                    variant='light'>
-                                        <Dropdown.Item as="button">Day</Dropdown.Item>
-                                        <Dropdown.Item as="button">Month</Dropdown.Item>
-                                        <Dropdown.Item as="button">Year</Dropdown.Item>
-                                    </DropdownButton>
-                                </Row>
-                                <Row>
-                                    <Col className='m-2 txt-white fs-1 fw-bold'>₱5,105,811.99</Col>
-                                </Row>
-                                <Row>
-                                    <Button className='rounded-4 w-auto txt-profit-dark fw-bold shadow' 
-                                            variant='light'>
-                                        <img src='icon_up_.png'></img>
-                                        +28%
-                                    </Button>
-                                </Row>
-                            </Card>
+                    <Row className='fw-bold txt-20 my-2'>
+                        Revenue
                     </Row>
-                    </Link>
+                    <Row className='me-2'>
+                        <Card className='p-4 bg-main-dominant-red overlay-revenue' onClick={navigateSales}>
+                            <Row>
+                                <Form.Select    size="sm"
+                                                className='w-auto ms-auto fw-bold'
+                                                onClick={handleDropdownClick}>
+                                    <option>Day</option>
+                                    <option>Month</option>
+                                    <option>Year</option>
+                                </Form.Select>
+                            </Row>
+                            <Row>
+                                <Col className='m-2 txt-white fs-1 fw-bold'>₱5,105,811.99</Col>
+                            </Row>
+                            <Row>
+                                <Button className='rounded-4 w-auto txt-profit-dark fw-bold shadow' 
+                                        variant='light'>
+                                    <img src='icon_up_.png'></img>
+                                    +28%
+                                </Button>
+                            </Row>
+                        </Card>
+                    </Row>
                 </Col>
 
                 <Col className='col-4 d-flex flex-column'>
