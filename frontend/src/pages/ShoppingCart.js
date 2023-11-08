4d7722c1583e8a872c33aa18a81a163a35ca7f0b
frncs-dc <97236341+frncs-dc@users.noreply.github.com>
2023-11-08 22:46:00 +0800

    Update ShoppingCart.js
    added layout foundation to shopping cart

--- frontend/src/pages/ShoppingCart.js
+++ frontend/src/pages/ShoppingCart.js
@@ -1,11 +1,25 @@
+import { Container, Row, Col, Card } from 'react-bootstrap'
+
 const ShoppingCart = () => {
 
     return (
-        <div className="shopping-cart">
-            <h1>THIS IS THE SHOPPING CART PAGE</h1>
-
-
-        </div>
+        <Container className='main'>
+            <Row className='fs-2 fw-bold'>
+                Check Out
+            </Row>
+            <Row>
+                <Card className='p-4 rounded-4 shadow mt-3'>
+                    <Row>
+                        <Col className='col-6 border'>
+                            inventory + add items here + navigation tools (this is ez just copy paste eh)
+                        </Col>
+                        <Col className='col-6 border'>
+                            remove from cart + purchase confirmation
+                        </Col>
+                    </Row>
+                </Card>
+            </Row>
+        </Container>
     )
 }
 
