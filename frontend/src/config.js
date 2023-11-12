706e129d2feb1cc0b2bb676b89c927ab4b3c3b41
Brian Gabini <91449132+nriab-dlsu@users.noreply.github.com>
2023-11-12 15:25:02 +0800

    test

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,11 +1,17 @@
+const { NODE_ENV, PREVIEW_BRANCH } = process.env;
+
 let DOMAIN;
 
-if (process.env.NODE_ENV === 'preview') {
-    DOMAIN = "https://jpdgarage-dev-api.vercel.app";  // Development domain
-} else if (process.env.NODE_ENV === 'production') {
+
+if (PREVIEW_BRANCH === 'dev') {
+    DOMAIN = "https://jpdgarage-dev-api.vercel.app";  // Development domain for preview
+} else if (NODE_ENV === 'production') {
     DOMAIN = "https://jpdgarage-api.vercel.app";  // Production domain
 } else {
     DOMAIN = "http://localhost:4000";  // Local testing domain
 }
+
+console.log('NODE_ENV:', NODE_ENV);
+console.log('DOMAIN:', DOMAIN);
 
 export { DOMAIN };
