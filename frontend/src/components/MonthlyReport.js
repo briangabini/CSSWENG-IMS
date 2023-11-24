2b92d6288d73c7b54125dac89d8d3ac4e4298355
DLSYU <hanz_yu@dlsu.edu.ph>
2023-11-25 00:40:15 +0800

    Componentization of Sales

--- frontend/src/components/MonthlyReport.js
+++ frontend/src/components/MonthlyReport.js
@@ -1,6 +1,16 @@
+import SalesGraph from "./SalesGraph";
+import SalesRank from "./SalesRank";
+import SalesQuota from "./SalesQuota";
+
+const reportPeriod = "Monthly";
+
 const MonthlyReport = () => {
     return (
         <>
+            <SalesGraph period= {reportPeriod} />
+
+            <SalesRank period= {reportPeriod}/>
+            <SalesQuota />
         </>
     )
 }
---
+++ frontend/src/components/SalesQuota.js
@@ -0,0 +1,9 @@
+const SalesQuota = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default SalesQuota
